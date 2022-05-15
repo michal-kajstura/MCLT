4449bc414c180d490ac8f5402ce32589dfe7417b
michal.kajstura <michal.kajstura@alphamoon.ai>
2022-05-15 18:15:33 +0200

    feat: add training scripts

---
+++ scripts/training/run_train_multilang.py
@@ -0,0 +1,29 @@
+import sys
+from copy import deepcopy
+
+from mclt import PROJECT_PATH
+from mclt.data import TASK_LANGUAGE_TABLE
+from mclt.utils.config import load_config
+from mclt.utils.experiments import (
+    create_datamodule,
+    create_multilingual_model_trainer,
+    run_experiment,
+)
+
+sys.path.append(PROJECT_PATH)
+
+config = load_config('train')
+config.update(load_config(config['method']))
+for task_name in TASK_LANGUAGE_TABLE.columns:
+    config['tasks'] = [task_name]
+    config['languages'] = None
+    for repeat in range(config['num_repeats']):
+        config = deepcopy(config)
+        config['model_random_state'] += repeat
+
+        run_experiment(
+            config,
+            create_datamodule=create_datamodule,
+            create_model_trainer=create_multilingual_model_trainer,
+            experiment_name='multi_language',
+        )
--- scripts/training/run_train_multitask.py
+++ scripts/training/run_train_multitask.py
@@ -12,22 +12,6 @@ from mclt.utils.experiments import (
 
 sys.path.append(PROJECT_PATH)
 
-config = load_config('train')
-config.update(load_config(config['method']))
-for task_name in TASK_LANGUAGE_TABLE.columns:
-    config['tasks'] = [task_name]
-    config['languages'] = None
-    for repeat in range(config['num_repeats']):
-        config = deepcopy(config)
-        config['model_random_state'] += repeat
-
-        run_experiment(
-            config,
-            create_datamodule=create_datamodule,
-            create_model_trainer=create_multilingual_model_trainer,
-            experiment_name='multi_language',
-        )
-
 
 config = load_config('train')
 config.update(load_config(config['method']))
